7889c483b7a044f5635620547177bca35209a7a5
Anant Singh Chauhan <anant.c@gormalone.com>
2024-09-24 21:27:40 +0530

    removed commented code

--- config/production.js
+++ config/production.js
@@ -1,12 +1,3 @@
-// config/production.js
-// module.exports = {
-//     dbUrl: 'mongodb+srv://enganantschauhan21:5XuS6sbIxNebQClM@clusteravalanche.rcsev.mongodb.net/LoggerDbAsc',
-//     collection: 'logs',
-//     port: 3000,
-//     logLevel: 'info'
-    
-// };
-
 module.exports = {
     dbUrl: process.env.DATABASE_URL,  // Database URL stored in an environment variable
     collection: 'logs',
